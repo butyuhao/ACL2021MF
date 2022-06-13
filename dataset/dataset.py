1dada9ad536b5878c69952b448ab0e0acb00e130
butyuhao <butyuhao@hotmail.com>
2022-06-13 13:13:13 +0800

    update

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -9,6 +9,188 @@ import re
 import string
 import sys
 
+
+keyword_len = 30
+
+class CommonGenDatasetP(Dataset):
+
+	def __init__(self, config, json_path, tokenizer, copy_vocab, decoder_start_token_id, is_training=False,
+				 attachable_index=None):
+		super(CommonGenDatasetP, self).__init__()
+
+		self.config = config
+		self.copy_vocab = copy_vocab
+		self.tokenizer = tokenizer
+		self.is_training = is_training
+		self.decoder_start_token_id = decoder_start_token_id
+		self.attachable_index = attachable_index
+		np.set_printoptions(threshold=sys.maxsize)
+		self.read_content(json_path)
+
+	def read_content(self, json_path):  # json文件 有几个概念词，然后有三句用概念词造的句子。
+		print("reading data from %s ..." % json_path)
+		self.record = []
+		with open(json_path) as out:
+			lines = out.readlines()
+			for l in tqdm(lines):
+				item = json.loads(l.strip())
+				concept_set = '生成文案 : '
+
+				expected_input_len = len(self.tokenizer('生成文案 : ' + ','.join(item['concept_set'].split('#')))['input_ids'])
+				expected_output_len = len(self.tokenizer(item['scene'])['input_ids'][0])
+				# 这边输入的keyword都会被替换成copy vocab里头的第一个，替换后如果文中没有出现，会导致在文中找不到词语，数据被丢掉的情况。
+				if expected_input_len > keyword_len:
+					continue
+				if expected_output_len > 512:
+					continue
+
+				concept_set_input_ids = self.tokenizer(concept_set, return_tensors="np")['input_ids'][0,
+										:-1].tolist()  # input_ids
+				concept_cls = []  # keyword的category id，也就是copy vocab中的行号
+				for concept in item['concept_set'].split('#'):
+					if concept in self.copy_vocab.word_to_category_id:  # 如果输入的词语在word_to_category_id里头，就在concept_cls放word_to_category_id的id
+						concept_cls.append(self.copy_vocab.word_to_category_id[concept])
+					else:  # 否则，直接在concept_cls中放词语的token id 那么concept_cls是用来干什么的呢？
+						fg_index = self.copy_vocab.w_to_i[concept]
+						concept_cls.append(self.copy_vocab.i_to_cls[fg_index])
+
+				start_pos = []  # 每个keyword在input里头的开始位置
+				for i, c_cls in enumerate(concept_cls):
+					start_pos.append(len(concept_set_input_ids))
+					concept_set_input_ids += self.copy_vocab.token_class[c_cls]
+					if i == len(concept_cls) - 1:
+						concept_set_input_ids.append(self.tokenizer.eos_token_id)
+				start_pos.append(len(concept_set_input_ids) - 1)
+
+				position_indicator = np.zeros((keyword_len, len(concept_set_input_ids)), dtype=np.float32)
+				for i in range(len(concept_cls)):
+					position_indicator[i, start_pos[i]:start_pos[i + 1]] = 1  # 关键词位置赋值为1
+				sum_check = np.sum(position_indicator, axis=1)
+				for i in range(len(concept_cls)):
+					assert sum_check[i] > 0
+
+				cls_on_input = np.zeros((len(concept_set_input_ids),), dtype=np.int64)  # 关键词的span上放上category id
+				for i, cls_ in enumerate(concept_cls):
+					cls_on_input[start_pos[i]:start_pos[i + 1]] = cls_
+
+				gt = copy.deepcopy(item['scene'])  # reference sequences
+				if self.is_training:
+					for c in item['scene']:
+						c = c.lower()
+						c_input_ids = self.tokenizer(c, return_tensors="np")['input_ids'][0]
+						string_caption = ' '.join([str(x) for x in c_input_ids])
+						if self.config.use_pointer:
+							for c_cls in concept_cls:
+								for _, fg_index in self.copy_vocab.d_to_w_group[c_cls]:
+									fg_word_index = self.copy_vocab.token_fg_w[fg_index]
+									fg_str = ' '.join([str(x) for x in fg_word_index])
+									fg_softmax_index = self.config.vocab_size + fg_index
+
+									string_caption = re.sub(' %s ' % fg_str, ' (%d) ' % fg_softmax_index,
+															string_caption)
+									string_caption = re.sub(' %s$' % fg_str, ' (%d)' % fg_softmax_index, string_caption)
+									string_caption = re.sub('^%s ' % fg_str, '(%d) ' % fg_softmax_index, string_caption)
+
+						c_input_ids = []
+						str_id_list = string_caption.split()  # labels
+						copy_mention_flag = np.zeros((len(str_id_list) + 1, keyword_len))
+						decoder_mention_flag = np.zeros(
+							(len(str_id_list), len(concept_set_input_ids)))  # [输出序列维度，输入序列维度]
+						use_this_record = True
+						if self.config.use_pointer:
+							for index, w in enumerate(str_id_list):
+								if w.startswith('(') and w.endswith(')'):
+									fg_index = int(w[1:-1])
+									cls_index = self.copy_vocab.i_to_cls[fg_index - self.config.vocab_size]
+									for j in range(len(concept_cls)):
+										if concept_cls[j] == cls_index:
+											copy_mention_flag[:index + 1, j] = 1
+											copy_mention_flag[index + 1:, j] = 2
+											decoder_mention_flag[:index + 1, start_pos[j]:start_pos[j + 1]] = 1
+											decoder_mention_flag[index + 1:, start_pos[j]:start_pos[j + 1]] = 2
+									c_input_ids.append(fg_index)
+
+									assert c_input_ids[-1] > self.config.vocab_size
+								else:
+									c_input_ids.append(int(w))
+									assert c_input_ids[-1] <= self.config.vocab_size
+						else:
+							for index, w in enumerate(str_id_list):
+								c_input_ids.append(int(w))
+								assert c_input_ids[-1] <= self.config.vocab_size  # 确保token id在词表范围内
+
+							for j, cls_index in enumerate(concept_cls):
+								if cls_index == 0: continue
+
+								for (_, fg_index) in self.copy_vocab.d_to_w_group[cls_index]:
+									fg_ch_list = self.copy_vocab.token_fg_w[fg_index]
+									s1 = '&'.join([str(f) for f in fg_ch_list])
+
+									for ch_idx, first_ch in enumerate(c_input_ids):
+										if first_ch == fg_ch_list[0]:
+											s2 = '&'.join(
+												[str(f) for f in c_input_ids[ch_idx: ch_idx + len(fg_ch_list)]])
+											if s1 == s2:  # 关键词和句子匹配上了
+												# if ch_idx + len(fg_ch_list) >= len(c_input_ids) - 1 or c_input_ids[
+												# 	ch_idx + len(fg_ch_list)] not in self.attachable_index: # 判断当前词语是不是已经结尾了，如果是string的结尾或者后一个词不可能和当前词语组成一个完整的词语的话(以__开头)，那么可以认为当前词语已经结束，而因为s1 == s2，所以将当前数据加入训练集。
+												decoder_mention_flag[:ch_idx + len(fg_ch_list),
+												start_pos[j]:start_pos[j + 1]] = 1
+												if not self.config.static_mf:
+													decoder_mention_flag[ch_idx + len(fg_ch_list):,
+													start_pos[j]:start_pos[j + 1]] = 2
+												else:
+													decoder_mention_flag[ch_idx + len(fg_ch_list):,
+													start_pos[j]:start_pos[j + 1]] = 1
+
+												break
+
+							if not self.config.static_mf:
+								for j in range(len(concept_cls)):
+									for jj in range(start_pos[j], start_pos[j + 1]):
+										if not decoder_mention_flag[-1, jj] == 2:  # 如果有关键词没有出现的，就跳过样例
+											use_this_record = False
+
+						if use_this_record:  # 没有被跳过的话就把样例放到instance_tuple里头去
+							instance_tuple = (
+							concept_set_input_ids, position_indicator, cls_on_input, concept_cls, copy_mention_flag,
+							decoder_mention_flag, c_input_ids, gt, item['concept_set'].split('#'))
+							self.record.append(instance_tuple)
+
+				else:  # 如果不是训练模式
+					copy_mention_flag = np.zeros((1, keyword_len))
+					copy_mention_flag[0, :len(concept_cls)] = 1
+					decoder_mention_flag = np.zeros((1, len(concept_set_input_ids)))
+					decoder_mention_flag[0, start_pos[0]: start_pos[-1]] = 1
+					self.record.append((concept_set_input_ids, position_indicator, cls_on_input, concept_cls,
+										copy_mention_flag, decoder_mention_flag, None, gt,
+										item['concept_set'].split('#')))
+
+		if self.is_training: random.shuffle(self.record)
+
+	def __len__(self):
+		return len(self.record)
+
+	def __getitem__(self, index):
+		concept_set, pos, cls_on_input, concept_cls, copy_mention_flag, decoder_mention_flag, gen, gt, gt_concept = \
+		self.record[index]
+
+		item = {
+			"gt": gt,
+			"gt_concepts": gt_concept,
+			"concept_set": concept_set,
+			"copy_pos": pos,
+			"concept_cls": concept_cls,
+			"copy_mention_flag": copy_mention_flag,
+			"decoder_mention_flag": decoder_mention_flag,
+			"cls_on_input": cls_on_input
+		}
+
+		if self.is_training:
+			item['gen'] = gen
+
+		return item
+
+
 class CommonGenDataset(Dataset):
 
 	def __init__(self, config, json_path, tokenizer, copy_vocab, decoder_start_token_id, is_training=False, attachable_index=None):
@@ -31,8 +213,8 @@ class CommonGenDataset(Dataset):
 			for l in tqdm(lines):
 				item = json.loads(l.strip())
 				concept_set = 'generate a sentence with these concepts : '
-				concept_set_input_ids = self.tokenizer(concept_set, return_tensors="np")['input_ids'][0, :-1].tolist()
-				concept_cls = []
+				concept_set_input_ids = self.tokenizer(concept_set, return_tensors="np")['input_ids'][0, :-1].tolist() # input_ids
+				concept_cls = [] # keyword的category id，也就是copy vocab中的行号
 				for concept in item['concept_set'].split('#'):
 					if concept in self.copy_vocab.word_to_category_id: # 如果输入的词语在word_to_category_id里头，就在concept_cls放word_to_category_id的id
 						concept_cls.append(self.copy_vocab.word_to_category_id[concept])
@@ -41,7 +223,7 @@ class CommonGenDataset(Dataset):
 						concept_cls.append(self.copy_vocab.i_to_cls[fg_index])
 
 				assert len(concept_cls) <= 5 # 输入的词语数量一定是小于等于5的
-				start_pos = []
+				start_pos = [] # 每个keyword在input里头的开始位置
 				for i, c_cls in enumerate(concept_cls):
 					start_pos.append(len(concept_set_input_ids))
 					concept_set_input_ids += self.copy_vocab.token_class[c_cls]
@@ -51,16 +233,16 @@ class CommonGenDataset(Dataset):
 
 				position_indicator = np.zeros((5, len(concept_set_input_ids)), dtype=np.float32)
 				for i in range(len(concept_cls)):
-					position_indicator[i, start_pos[i]:start_pos[i+1]] = 1
+					position_indicator[i, start_pos[i]:start_pos[i+1]] = 1 # 关键词位置赋值为1
 				sum_check = np.sum(position_indicator, axis=1)
 				for i in range(len(concept_cls)):
 					assert sum_check[i] > 0
 
-				cls_on_input = np.zeros((len(concept_set_input_ids), ), dtype=np.int64)
+				cls_on_input = np.zeros((len(concept_set_input_ids), ), dtype=np.int64) # 关键词的span上放上category id
 				for i, cls_ in enumerate(concept_cls):
 					cls_on_input[start_pos[i]:start_pos[i+1]] = cls_
 
-				gt = copy.deepcopy(item['scene'])
+				gt = copy.deepcopy(item['scene']) # reference sequences
 				if self.is_training:
 					for c in item['scene']:
 						c = c.lower()
@@ -78,9 +260,9 @@ class CommonGenDataset(Dataset):
 									string_caption = re.sub('^%s ' % fg_str, '(%d) ' % fg_softmax_index, string_caption)
 						
 						c_input_ids = []
-						str_id_list = string_caption.split()
+						str_id_list = string_caption.split() # labels
 						copy_mention_flag = np.zeros((len(str_id_list) + 1, 5))
-						decoder_mention_flag = np.zeros((len(str_id_list), len(concept_set_input_ids)))
+						decoder_mention_flag = np.zeros((len(str_id_list), len(concept_set_input_ids))) # [输出序列维度，输入序列维度]
 						use_this_record = True
 						if self.config.use_pointer:
 							for index, w in enumerate(str_id_list):
@@ -102,40 +284,44 @@ class CommonGenDataset(Dataset):
 						else:
 							for index, w in enumerate(str_id_list):
 								c_input_ids.append(int(w))
-								assert c_input_ids[-1] <= self.config.vocab_size
+								assert c_input_ids[-1] <= self.config.vocab_size # 确保token id在词表范围内
 
 							for j, cls_index in enumerate(concept_cls):
 								if cls_index == 0: continue
-
-								for (_, fg_index) in self.copy_vocab.d_to_w_group[cls_index]:
+								'''
+								如果按组的话，拿到一个concept，会在文本搜索同一组的所有词有没有在文中出现。
+								但是如果不按组构建copy_vocab的话，如果concept和文本中不一样，就检测不到。
+								'''
+								for (_, fg_index) in self.copy_vocab.d_to_w_group[cls_index]: # 这边是遍历相似的词语
 									fg_ch_list = self.copy_vocab.token_fg_w[fg_index]
 									s1 = '&'.join([str(f) for f in fg_ch_list])
+
 
 									for ch_idx, first_ch in enumerate(c_input_ids):
 										if first_ch == fg_ch_list[0]:
 											s2 = '&'.join([str(f) for f in c_input_ids[ch_idx: ch_idx + len(fg_ch_list)]])
-											if s1 == s2:
-												if ch_idx + len(fg_ch_list) >= len(c_input_ids) - 1 or c_input_ids[ch_idx + len(fg_ch_list)] not in self.attachable_index:
-													decoder_mention_flag[:ch_idx + len(fg_ch_list), start_pos[j]:start_pos[j+1]] = 1
-													if not self.config.static_mf:
-														decoder_mention_flag[ch_idx + len(fg_ch_list):, start_pos[j]:start_pos[j+1]] = 2
-													else:
-														decoder_mention_flag[ch_idx + len(fg_ch_list):, start_pos[j]:start_pos[j+1]] = 1
-													
-													break
+											if s1 == s2: # 关键词和句子匹配上了
+												# if ch_idx + len(fg_ch_list) >= len(c_input_ids) - 1 or c_input_ids[ch_idx + len(fg_ch_list)] not in self.attachable_index: # attachable_index 里面放的是可以自己开头的词语，也就是以__开头的，这边是要判断是不是当前词语的结尾。如果后一个词可以自己开头，则可以判定当前词语不是别的某个词语的前缀。
+												decoder_mention_flag[:ch_idx + len(fg_ch_list), start_pos[j]:start_pos[j+1]] = 1
+												if not self.config.static_mf:
+													decoder_mention_flag[ch_idx + len(fg_ch_list):, start_pos[j]:start_pos[j+1]] = 2
+												else:
+													decoder_mention_flag[ch_idx + len(fg_ch_list):, start_pos[j]:start_pos[j+1]] = 1
+
+												break
 							
 							if not self.config.static_mf:		
 								for j in range(len(concept_cls)):
 									for jj in range(start_pos[j], start_pos[j+1]):
-										if not decoder_mention_flag[-1, jj] == 2:
+										if not decoder_mention_flag[-1, jj] == 2: # 如果有关键词没有出现的，就跳过样例
 											use_this_record = False
 	
-						if use_this_record:
+						if use_this_record: # 没有被跳过的话就把样例放到instance_tuple里头去
 							instance_tuple = (concept_set_input_ids, position_indicator, cls_on_input, concept_cls, copy_mention_flag, decoder_mention_flag, c_input_ids, gt, item['concept_set'].split('#'))
 							self.record.append(instance_tuple)
 					
-				else:
-					copy_mention_flag = np.zeros((1, 5))
+				else: # 如果不是训练模式
+					copy_mention_flag = np.zeros((1, keyword_len))
 					copy_mention_flag[0, :len(concept_cls)] = 1
 					decoder_mention_flag = np.zeros((1, len(concept_set_input_ids)))
 					decoder_mention_flag[0, start_pos[0]: start_pos[-1]] = 1
@@ -184,12 +370,12 @@ def data_wrapper(dataset, tokenizer, decoder_start_token_id):
 	new_dataset['cls_on_input'] = torch.from_numpy(cls_on_input)
 	new_dataset['attention_mask'] = (new_dataset['input_ids'] != _PAD).float()
 
-	copy_pos = np.zeros((batch_size, 5, max_concept_len), dtype=np.float32)
+	copy_pos = np.zeros((batch_size, keyword_len, max_concept_len), dtype=np.float32)
 	for i, d in enumerate(dataset):
 		copy_pos[i, :, :d['copy_pos'].shape[-1]] = d['copy_pos']
 	new_dataset['copy_pos'] = torch.from_numpy(copy_pos)
 
-	concept_cls = np.zeros((batch_size, 5), dtype=np.int64)
+	concept_cls = np.zeros((batch_size, keyword_len), dtype=np.int64)
 	for i, d in enumerate(dataset):
 		concept_cls[i, :len(d['concept_cls'])] = d['concept_cls']
 	new_dataset['concept_cls'] = torch.from_numpy(concept_cls)
@@ -207,7 +393,7 @@ def data_wrapper(dataset, tokenizer, decoder_start_token_id):
 		new_dataset['decoder_input_ids'] = torch.from_numpy(gen_input_seqs)
 		new_dataset['decoder_input_mask'] = (torch.from_numpy(gen_input_seqs) != _EOS).bool()
 
-	copy_mention_flag = np.zeros((batch_size, max_gen_len, 5), dtype=np.int64)
+	copy_mention_flag = np.zeros((batch_size, max_gen_len, keyword_len), dtype=np.int64)
 	for i, d in enumerate(dataset):
 		copy_mention_flag[i, :d['copy_mention_flag'].shape[0]] = d['copy_mention_flag']
 	new_dataset['copy_mention_flag'] = torch.from_numpy(copy_mention_flag)
